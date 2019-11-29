b1a91ecb0ffb2035dd50f83e5557335a5a4cd9c0
Reuben Leibowitz <reubenc4pro@outlook.com>
2019-11-29 19:32:07 +0200

    Update Selection_Behaviour.cs

--- Selection/Selection_Behaviour.cs
+++ Selection/Selection_Behaviour.cs
@@ -1,7 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Selection_Behaviour : MonoBehaviour
 {
 	public bool DragSelection = true;
+	public bool Selected = false;
+	public Color Initial;
+	public Color AlternativeColour;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,6 +17,15 @@ public class Selection_Behaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Selected)
+		{
+			if (tag == "Rock")
+				GetComponent<Renderer>().material.color = AlternativeColour;
+		}
+		else
+		{
+			if (tag == "Rock")
+				GetComponent<Renderer>().material.color = Initial;
+		}
     }
 }
